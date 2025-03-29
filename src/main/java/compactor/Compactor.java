19b4a83b71c4f5acacdcb23043a799d42eace387
Peziiim <eduardoorx@gmail.com>
2025-03-29 17:34:50 -0300

    fixed compactor

--- src/main/java/compactor/Compactor.java
+++ src/main/java/compactor/Compactor.java
@@ -1,50 +1,68 @@
 package compactor;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.io.*;
+import java.util.zip.*;
 
 public class Compactor {
 
-    static final int BUFFER_KB = 4096;
+    private static final int BUFFER_KB = 4096;
 
-    public void toZip(String arquiveEntry, String arquiveOut) {
-
-        int count;
-        byte[] data = new byte[BUFFER_KB];
-
-        BufferedInputStream origin = null;
-        FileInputStream entryStream = null;
-        FileOutputStream destiny = null;
-        ZipOutputStream out = null;
-        ZipEntry entry = null;
-
+    public void toZip(String folderPath, String zipPath) {
+        
+        File folder = new File(folderPath);
+        
         try {
-
-            destiny = new FileOutputStream(new File(arquiveOut));
-            out = new ZipOutputStream(new BufferedOutputStream(destiny));
-            File file = new File(arquiveEntry);
-            entryStream = new FileInputStream(file);
-            origin = new BufferedInputStream(entryStream, BUFFER_KB);
-            entry = new ZipEntry(file.getName());
-            out.putNextEntry(entry);
-
-            while ((count = origin.read(data, 0, BUFFER_KB)) != -1) {
-            out.write(data, 0, count);
+            if (!folder.exists()) {
+                System.out.println("Erro: pasta não encontrada ");
+                throw new IOException();
             }
 
-            origin.close();
-            out.close();
-
-        } catch (IOException e) {
-            System.out.println("Erro tentando encontrar o arquivo: " + e.getMessage());
+        } catch (IOException e){
             e.printStackTrace();
         }
 
+        File zipFile = new File(zipPath);
+        zipFile.getParentFile().mkdirs();
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(fileOutputStream));
+            zipFolder(folder, folder.getName(), zipOutputStream);
+
+        } catch (IOException e) {
+            System.out.println("Falha ao criar arquivo " + e.getMessage());
+        }
+    }
+
+    private void zipFolder(File folder, String basePath, ZipOutputStream zipOutputStream) throws IOException {
+        File[] files = folder.listFiles();
+        if (files == null) return; 
+
+        byte[] buffer = new byte[BUFFER_KB];
+
+        for (File file : files) {
+            String zipEntryName = basePath + "/" + file.getName();
+            if (file.isDirectory()) {
+                zipFolder(file, zipEntryName, zipOutputStream); 
+            } else {
+                try{ 
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    BufferedInputStream origin = new BufferedInputStream(fileInputStream, BUFFER_KB);
+                    ZipEntry entry = new ZipEntry(zipEntryName);
+                    zipOutputStream.putNextEntry(entry);
+
+                    int count;
+                    while ((count = origin.read(buffer)) != -1) {
+                        zipOutputStream.write(buffer, 0, count);
+                    } 
+
+                    } catch (IOException e) {
+                     System.out.println("Erro ao compactar a pasta " + e.getMessage());
+
+                    } finally {
+                        zipOutputStream.closeEntry();
+                    }
+            }
+        }
     }
 }
--- src/test/java/maintest/AppTest.java
+++ src/test/java/maintest/AppTest.java
@@ -9,22 +9,35 @@ import java.nio.file.Path;
 
 import org.junit.Test;
 
+import compactor.Compactor;
 import scraping.Scraping;
 
 public class AppTest {
-
+    private Compactor compactor = new Compactor();
     private Scraping scraping = new Scraping();
-    private  Path savePath = Path.of("src/test/java/filestest/dummy.pdf");
+    private Path path = Path.of("src/test/java/filestest/dummy.pdf");
 
     @Test
     public void testDownloadFile() throws IOException, URISyntaxException {
         
         String fileUrl = "https://www.unirio.br/cchs/eb/arquivos/tccs-acima-de-9/TCC-%20Ana%20Paula%20Oliveira%20Jacques.pdf/at_download/file";
        
-        scraping.downloadFile(fileUrl, savePath);
-        assertTrue(Files.exists(savePath));
+        scraping.downloadFile(fileUrl, path);
+        assertTrue(Files.exists(path));
 
         // Comente a linha abaixo para ver o arquivo guardado na pasta
-        Files.deleteIfExists(savePath);
+        Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void testToZip() throws IOException{
+        String stringPath = path.toString();
+        compactor.toZip(stringPath, stringPath + ".zip");
+
+        assertTrue(Files.exists(Path.of(path + ".zip")));
+
+         // Comente a linha abaixo para ver o arquivo guardado na pasta
+         Files.deleteIfExists(path);
+    
     }
 }
